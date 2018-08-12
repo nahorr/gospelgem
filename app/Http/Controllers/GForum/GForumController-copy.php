9edb0a4270d21faf9ef6783513e0c21c89aa01af
Nnamdi <nahorr@gmail.com>
2018-08-12 11:12:10 -0600

    save summernot img

--- app/Http/Controllers/GForum/GForumController-copy.php
+++ app/Http/Controllers/GForum/GForumController-copy.php
@@ -36,11 +36,40 @@ class GForumController extends Controller
         ]);
 
 
+        $post_body=$request->input('post_body');
+        $dom = new \DomDocument();
+        $dom->loadHtml($post_body, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+        $images = $dom->getElementsByTagName('img');
+
+        foreach($images as $k => $img){
+
+            $data = $img->getAttribute('src');
+
+            list($type, $data) = explode(';', $data);
+
+            list(, $data)      = explode(',', $data);
+
+            $data = base64_decode($data);
+
+            $image_name= "/posts/images/" . time().$k.'.png';
+
+            $path = public_path() . $image_name;
+
+            file_put_contents($path, $data);
+
+            $img->removeAttribute('src');
+
+            $img->setAttribute('src', $image_name);
+
+        }
+
+        $post_body = $dom->saveHTML();
+
         Post::insert([
                     'user_id'=>$request->user_id,
                     'category_id'=>$request->category_id,
                     'post_title'=>$request->post_title,
-                    'post_body'=>$request['post_body'],
+                    'post_body'=>$post_body,
                     'show_profile_picture' => $request->show_profile_picture,
                     'created_at' => date('Y-m-d H:i:s'),
                     'updated_at' => date('Y-m-d H:i:s'),
@@ -64,13 +93,40 @@ class GForumController extends Controller
             'post_body' => 'required',
         ]);
               
-        
+        $post_body=$request->input('post_body');
+        $dom = new \DomDocument();
+        $dom->loadHtml($post_body, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+        $images = $dom->getElementsByTagName('img');
+
+        foreach($images as $k => $img){
+
+            $data = $img->getAttribute('src');
+
+            list($type, $data) = explode(';', $data);
+
+            list(, $data)      = explode(',', $data);
+
+            $data = base64_decode($data);
+
+            $image_name= "/posts/images/" . time().$k.'.png';
+
+            $path = public_path() . $image_name;
+
+            file_put_contents($path, $data);
+
+            $img->removeAttribute('src');
+
+            $img->setAttribute('src', $image_name);
+
+        }
+        $post_body = $dom->saveHTML();
+
         
 
         $post_edit = Post::where('id', '=', $post->id)->first();
         
         $post_edit->post_title= $request->post_title;
-        $post_edit->post_body= $request['post_body'];
+        $post_edit->post_body= $post_body;
         $post_edit->show_profile_picture= $request->show_profile_picture;
         $post_edit->updated_at= date('Y-m-d H:i:s');
         $post_edit->save();
