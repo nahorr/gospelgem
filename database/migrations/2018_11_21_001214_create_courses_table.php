e1bcc395b0e7217bd03d0f2b0372e321eb90366b
nahorr <nahorr@gmail.com>
2018-11-20 20:09:18 -0700

    course categories and courses

---
+++ database/migrations/2018_11_21_001214_create_courses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('course_category_id')->unsigned();
+            $table->foreign('course_category_id')->references('id')->on('course_categories')->onDelete('cascade');
+            $table->string('course_code');
+            $table->string('course_name');
+            $table->string('course_picture');
+            $table->string('course_description');
+            $table->string('course_moodle_link')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
