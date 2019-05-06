64fb12f475069248a111c93f88ab4e87960cab9a
Nnamdi Okeke <nahorr@gmail.com>
2019-05-05 22:35:29 -0600

    add meta crsf to header

--- resources/views/layouts/private/app.blade.php
+++ resources/views/layouts/private/app.blade.php
@@ -10,7 +10,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <!-- Favicon -->
   <link rel="shortcut icon" href="{{asset('unify/favicon_private.png')}}">
   <!-- Google Fonts -->
