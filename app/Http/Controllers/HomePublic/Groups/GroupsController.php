10d784cb36bd8a87d1e3486b16f51a3d4e197b34
Nnamdi Okeke <nahorr@gmail.com>
2019-08-16 08:32:29 -0600

    groups page

--- app/Http/Controllers/HomePublic/Groups/GroupsController.php
+++ app/Http/Controllers/HomePublic/Groups/GroupsController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\HomePublic\Groups;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Group;
 
 class GroupsController extends Controller
 {
     public function show()
     {
-    	return view('public-views.groups.show');
+    	$groups = Group::get();
+
+    	return view('public-views.groups.show', compact('groups'));
     }
 }
--- resources/views/public-views/groups/show.blade.php
+++ resources/views/public-views/groups/show.blade.php
@@ -4,13 +4,38 @@
   
   @include('layouts.public.includes.nav-header')
 
-  <!--Basic Table-->
-  <section class="g-py-100">
-    <div class="container">
-    
-    </div>
-  </section>
-  <!--End Basic Table-->
+  <!-- Popular Jobs -->
+      <section class="g-py-100">
+        <div class="container">
+          <header class="text-center g-width-60x--md mx-auto g-mb-50">
+            <h2 class="h1 g-color-gray-dark-v1 g-font-weight-300">Gosyan Groups({{$groups->count()}})</h2>
+            <p class="lead">Please contact group admin or send a join request to the admin if you wish to join the group.</p>
+          </header>
 
+          <div class="row g-mb-30">
+            @foreach($groups as $group)
+            <div class="col-lg-4 g-mb-40 g-mb-0--lg">
+              <ul class="list-unstyled mb-0">
+                <li class="media u-shadow-v11 rounded g-pa-20 g-mb-10">
+                  <div class="d-flex align-self-center g-mt-3 g-mr-15">
+                    <img class="g-width-40 g-height-40" src="{{asset('unify/assets/img-temp/logos/img1.png')}}" alt="Image Description">
+                  </div>
+                  <div class="media-body">
+                    <a class="d-block u-link-v5 g-color-main g-color-primary--hover g-font-weight-600 g-mb-3" href="">{{$group->name}}</a>
+                    <span class="g-font-size-13 g-color-gray-dark-v4 g-mr-15">
+                        <i class="icon-people g-pos-rel g-top-1 mr-1"></i> {{$group->users()->count()}} Members
+                      </span>
+                    <span class="g-font-size-13 g-color-gray-dark-v4 g-mr-15">
+                        <i class="icon-plus g-pos-rel g-top-1 mr-1"></i> Join
+                      </span>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            @endforeach
+          </div>
+        </div>
+      </section>
+      <!-- End Popular Jobs -->
   @include('layouts.public.includes.footer')
   @endsection
