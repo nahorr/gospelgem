6f93a26ada41c15d290f4f3d088160461d1d6f53
Nnamdi Okeke <nahorr@gmail.com>
2019-09-22 07:45:11 -0600

    count group members

--- resources/views/public-views/groups/show.blade.php
+++ resources/views/public-views/groups/show.blade.php
@@ -28,10 +28,7 @@
                     <a class="d-block u-link-v5 g-color-main g-color-primary--hover g-font-weight-600 g-mb-3" href="{{url('/groups/showgroupposts/'.$group->id)}}">{{$group->name}}</a>
                     <span class="g-font-size-13 g-color-gray-dark-v4 g-mr-15">                     
                         <i class="icon-people g-pos-rel g-top-1 mr-1"></i> 
-                        
-                            {{$group->users->count()}} Members
-                            <?php echo $group->users ?>
-                          
+                            {{$group->users->count()}} Members 
                     </span>
                     @if( in_array(\Auth::user()->id, $group->users->pluck('id')->toArray()))
                       <span class="g-font-size-13 g-color-gray-dark-v4 g-mr-15">
